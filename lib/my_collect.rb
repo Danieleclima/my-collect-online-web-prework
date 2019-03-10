d465c780b2bc356fe406f8930469387f8b4f8474
Daniel Lima <daniel.echuaca.lima@gmail.com>
2019-03-10 16:57:22 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,12 +1,25 @@
+<<<<<<< HEAD
 
+=======
+require "pry"
+>>>>>>> 90d427fe8336df7a1b8b24701cc7dc2d752c3c9a
 def my_collect (array)
      i = 0
     new_array = []
   while i < array.length
+<<<<<<< HEAD
     new_array << yield(array[i])
     i += 1
   end
   new_array
+=======
+    yield array[i]
+    new_array << array.first
+    binding.pry
+    i += 1
+  end
+  return new_array
+>>>>>>> 90d427fe8336df7a1b8b24701cc7dc2d752c3c9a
 end
   
 
